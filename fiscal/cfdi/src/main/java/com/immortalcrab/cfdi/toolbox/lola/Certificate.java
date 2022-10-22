811718ab131598c203c4fc629698c50405daad94
Edwin Pĺauchu <edwin.plauchu@wunderkind.co>
2022-10-22 09:13:46 -0500

    montes_toolbox_4_cfdi

---
+++ fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/toolbox/lola/Certificate.java
@@ -0,0 +1,17 @@
+package com.immortalcrab.cfdi.toolbox.lola;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import org.apache.commons.codec.binary.Base64;
+
+class Certificate {
+
+    public static String read(byte[] chunk) throws UnsupportedEncodingException, IOException {
+
+        return new String(
+                Base64.encodeBase64(chunk),
+                StandardCharsets.UTF_8.name()
+        );
+    }
+}
---
+++ fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/toolbox/lola/MontesToolbox.java
@@ -0,0 +1,27 @@
+package com.immortalcrab.cfdi.toolbox.lola;
+
+import com.immortalcrab.cfdi.toolbox.IToolbox;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import javax.xml.transform.Source;
+
+public class MontesToolbox implements IToolbox {
+
+    @Override
+    public String renderCerticate(byte[] chunk) throws UnsupportedEncodingException, IOException {
+        return Certificate.read(chunk);
+    }
+
+    @Override
+    public String renderOriginal(BufferedReader br, Source srcXslt) throws Exception {
+        return OriginalStrExtractor.read(br, srcXslt);
+    }
+
+    @Override
+    public String signOriginal(BufferedReader brPrivKeyPem, String original) throws Exception {
+
+        return Signer.sign(brPrivKeyPem, original);
+    }
+
+}
