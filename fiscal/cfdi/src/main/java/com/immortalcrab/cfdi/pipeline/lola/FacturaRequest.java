11667bb4ae17ccdbf60a3660169e78a6877f8f6e
Edwin Plauchu <pianodaemon@gmail.com>
2022-10-29 12:49:28 -0500

    Update FacturaRequest.java

--- fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/lola/FacturaRequest.java
+++ fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/lola/FacturaRequest.java
@@ -1,32 +1,19 @@
 package com.immortalcrab.cfdi.pipeline.lola;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.immortalcrab.cfdi.error.DecodeError;
 import com.immortalcrab.cfdi.error.RequestError;
-import com.immortalcrab.cfdi.pipeline.Request;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Map;
+import lombok.extern.apachecommons.CommonsLog;
 
-public class FacturaRequest extends Request {
+@CommonsLog
+public class FacturaRequest extends JsonRequest {
 
-    public static FacturaRequest render(InputStreamReader reader) throws RequestError, DecodeError, IOException {
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        TypeReference<Map<String, Object>> tr = new TypeReference<Map<String, Object>>() {
-        };
-        
-        return new FacturaRequest(mapper.readValue(reader, tr));
+    private FacturaRequest(InputStreamReader reader) throws RequestError, DecodeError {
+        super(reader);
     }
 
-    public FacturaRequest(Map<String, Object> ds) {
-        super(ds);
-    }
-
-    @Override
-    protected Map<String, Object> craftImpt() throws RequestError {
-        throw new UnsupportedOperationException("Not supported and needed yet.");
+    public static FacturaRequest render(InputStreamReader reader) throws RequestError, DecodeError {
+        FacturaRequest facturaRequest = new FacturaRequest(reader);
+        return facturaRequest;
     }
 }
