d636d355507dd2bd5e3f71c057255d53facf5f4f
Edwin Plauchu <pianodaemon@gmail.com>
2022-10-29 14:29:02 -0500

    Update FacturaRequest.java

--- fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/lola/FacturaRequest.java
+++ fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/lola/FacturaRequest.java
@@ -1,11 +1,9 @@
-package com.immortalcrab.cfdi.pipeline.lola;
+ppackage com.immortalcrab.cfdi.pipeline.lola;
 
 import com.immortalcrab.cfdi.error.DecodeError;
 import com.immortalcrab.cfdi.error.RequestError;
 import java.io.InputStreamReader;
-import lombok.extern.apachecommons.CommonsLog;
 
-@CommonsLog
 public class FacturaRequest extends JsonRequest {
 
     private FacturaRequest(InputStreamReader reader) throws RequestError, DecodeError {
