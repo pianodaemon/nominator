a069b31f45c042fdd059266a1a1503904c5a9965
Edwin Pĺauchu <edwin.plauchu@wunderkind.co>
2022-10-30 16:05:51 -0600

    plus

--- fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/lola/FacturaXml.java
+++ fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/lola/FacturaXml.java
@@ -1,11 +1,23 @@
 package com.immortalcrab.cfdi.pipeline.lola;
 
 import com.immortalcrab.cfdi.error.FormatError;
+import com.immortalcrab.cfdi.pipeline.Request;
+import com.immortalcrab.cfdi.pipeline.IStorage;
 import mx.gob.sat.cfd._4.ObjectFactory;
 import mx.gob.sat.sitio_internet.cfd.catalogos.CTipoDeComprobante;
 import java.io.StringWriter;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+
+@AllArgsConstructor
+@Getter
 public class FacturaXml {
+
+    private final @NonNull Request cfdiReq;
+
+    private final @NonNull IStorage st;
 
     private StringWriter shape() throws FormatError {
 
