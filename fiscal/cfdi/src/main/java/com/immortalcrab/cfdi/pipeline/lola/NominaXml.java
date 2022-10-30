6b5e2d9ef005a343bf1d6185fbc92a8c0d9770b5
Edwin Plauchu <pianodaemon@gmail.com>
2022-10-30 16:17:30 -0600

    Update NominaXml.java

--- fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/lola/NominaXml.java
+++ fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/lola/NominaXml.java
@@ -1,5 +1,6 @@
 package com.immortalcrab.cfdi.pipeline.lola;
 
+import com.immortalcrab.cfdi.error.StorageError;
 import com.immortalcrab.cfdi.error.FormatError;
 import com.immortalcrab.cfdi.pipeline.Request;
 import com.immortalcrab.cfdi.pipeline.IStorage;
@@ -16,8 +17,15 @@ import lombok.NonNull;
 public class NominaXml {
 
     private final @NonNull Request cfdiReq;
-
     private final @NonNull IStorage st;
+
+    public static String render(Request cfdiReq, IStorage st) throws FormatError, StorageError {
+
+        FacturaXml ic = new FacturaXml(cfdiReq, st);
+        StringWriter cfdi = ic.shape();
+
+        return "It must be slightly implemented as it was in lola";
+    }
 
     private StringWriter shape() throws FormatError {
 
