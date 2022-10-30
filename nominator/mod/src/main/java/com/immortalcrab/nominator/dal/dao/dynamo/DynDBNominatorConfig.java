e87889c2ae895680a4c6bdc9d9dbbeea18f7f773
Edwin Pĺauchu <edwin.plauchu@wunderkind.co>
2022-10-30 12:26:31 -0600

    dynamo_config_for_testing_separated

---
+++ nominator/mod/src/main/java/com/immortalcrab/nominator/dal/dao/dynamo/DynDBNominatorConfig.java
@@ -0,0 +1,39 @@
+package com.immortalcrab.nominator.dal.dao.dynamo;
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+
+//import lombok.NonNull;
+import lombok.Getter;
+
+@Getter
+class DynDBNominatorConfig {
+
+    private DynamoDBMapper mapper;
+    private AmazonDynamoDB dynamoDB;
+
+    public DynDBNominatorConfig(){
+        dynamoDB = createAmazonDynamoDBClient();
+        mapper = new DynamoDBMapper(dynamoDB);
+    }
+
+    private AmazonDynamoDB createAmazonDynamoDBClient() {
+
+        return AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8000",
+                        Regions.US_EAST_2.getName()))
+                .withCredentials(credentialsProvider())
+                .build();
+    }
+
+    private AWSStaticCredentialsProvider credentialsProvider() {
+
+        BasicAWSCredentials creds = new BasicAWSCredentials("fakeId", "fakeSecret");
+        return new AWSStaticCredentialsProvider(creds);
+    }
+}
