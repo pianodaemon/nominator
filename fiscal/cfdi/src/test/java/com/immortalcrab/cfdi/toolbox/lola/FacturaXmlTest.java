0f3fb17a8e38b4b20f92515a5c39fa2d122dfefa
Edwin Plauchu <pianodaemon@gmail.com>
2022-10-30 17:59:30 -0600

    Create FacturaXmlTest.java

---
+++ fiscal/cfdi/src/test/java/com/immortalcrab/cfdi/toolbox/lola/FacturaXmlTest.java
@@ -0,0 +1,31 @@
+package com.immortalcrab.cfdi.toolbox.lola;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+import com.immortalcrab.cfdi.pipeline.lola.FacturaXml;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class FacturaXmlTest {
+
+    @BeforeAll
+    public void setUpClass() {
+    }
+
+    @AfterAll
+    public void tearDownClass() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+}
