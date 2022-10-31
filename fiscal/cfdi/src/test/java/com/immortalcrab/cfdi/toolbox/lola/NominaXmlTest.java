84868cdc59ceb30e0e59ff6b7dab7f16aabef2f0
Edwin Plauchu <pianodaemon@gmail.com>
2022-10-30 18:20:34 -0600

    Update NominaXmlTest.java

--- fiscal/cfdi/src/test/java/com/immortalcrab/cfdi/toolbox/lola/NominaXmlTest.java
+++ fiscal/cfdi/src/test/java/com/immortalcrab/cfdi/toolbox/lola/NominaXmlTest.java
@@ -1,11 +1,20 @@
 package com.immortalcrab.cfdi.toolbox.lola;
 
+import com.immortalcrab.cfdi.error.DecodeError;
+import com.immortalcrab.cfdi.error.RequestError;
+import com.immortalcrab.cfdi.pipeline.lola.NominaRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.Test;
 
 import com.immortalcrab.cfdi.pipeline.lola.NominaXml;
 
@@ -26,6 +35,20 @@ public class NominaXmlTest {
 
     @AfterEach
     public void tearDown() {
+    }
+
+    @Test
+    void loadReqFromReader() {
+
+        try {
+            InputStream is = getClass().getClassLoader().getResourceAsStream("jsonreqs");
+            InputStreamReader reader = new InputStreamReader(is, StandardCharsets.UTF_8);
+
+            NominaRequest.render(reader);
+
+        } catch (RequestError | DecodeError ex) {
+            assertNotNull(ex);
+        }
     }
 
 }
