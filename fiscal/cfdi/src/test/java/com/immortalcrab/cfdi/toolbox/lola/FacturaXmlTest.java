a56f2b980022074ef17752de01109bee6653794a
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-01 12:42:59 -0600

    Update FacturaXmlTest.java

--- fiscal/cfdi/src/test/java/com/immortalcrab/cfdi/toolbox/lola/FacturaXmlTest.java
+++ fiscal/cfdi/src/test/java/com/immortalcrab/cfdi/toolbox/lola/FacturaXmlTest.java
@@ -1,19 +1,33 @@
 package com.immortalcrab.cfdi.toolbox.lola;
 
+import com.immortalcrab.cfdi.error.DecodeError;
+import com.immortalcrab.cfdi.error.RequestError;
+import com.immortalcrab.cfdi.pipeline.lola.FacturaRequest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 import com.immortalcrab.cfdi.pipeline.lola.FacturaXml;
 
 @TestInstance(Lifecycle.PER_CLASS)
 public class FacturaXmlTest {
 
+    private ClassLoader _cloader;
+
     @BeforeAll
     public void setUpClass() {
+
+        _cloader = getClass().getClassLoader();
     }
 
     @AfterAll
@@ -28,4 +42,18 @@ public class FacturaXmlTest {
     public void tearDown() {
     }
 
+    @Test
+    public void loadReqFromReader() {
+
+        try {
+
+            InputStream is = _cloader.getResourceAsStream("jsonreqs/facturareq.json");
+            InputStreamReader reader = new InputStreamReader(is, StandardCharsets.UTF_8);
+
+            FacturaRequest.render(reader);
+
+        } catch (RequestError | DecodeError ex) {
+            assertNotNull(ex);
+        }
+    }
 }
