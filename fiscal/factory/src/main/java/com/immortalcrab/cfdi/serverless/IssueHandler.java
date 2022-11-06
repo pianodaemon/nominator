9cae09ef4ea6ef915b815cddb4986ed90fd9d5c0
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-06 14:27:32 -0600

    Update IssueHandler.java

--- fiscal/factory/src/main/java/com/immortalcrab/cfdi/serverless/IssueHandler.java
+++ fiscal/factory/src/main/java/com/immortalcrab/cfdi/serverless/IssueHandler.java
@@ -4,8 +4,6 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 
-import com.immortalcrab.cfdi.serverless.XmlProducer;
-
 import lombok.extern.log4j.Log4j;
 
 @Log4j
