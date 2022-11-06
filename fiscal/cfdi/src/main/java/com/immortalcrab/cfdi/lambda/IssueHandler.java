c65bbbaf21f932c87b1f187ca117beffad0cd5ed
Edwin Pĺauchu <edwin.plauchu@wunderkind.co>
2022-11-05 20:49:20 -0600

    lambda_handler

---
+++ fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/lambda/IssueHandler.java
@@ -0,0 +1,21 @@
+package com.immortalcrab.cfdi.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class IssueHandler implements RequestHandler<SQSEvent, Void> {
+
+    @Override
+    public Void handleRequest(SQSEvent event, Context context) {
+
+        event.getRecords().forEach(msg -> {
+
+            log.info(msg.getBody());
+        });
+        return null;
+    }
+}
