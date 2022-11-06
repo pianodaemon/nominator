dc474117da5c435af708d6283da37ae7ce63f45c
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-06 15:00:16 -0600

    Update IXmlStep.java

--- fiscal/engine/src/main/java/com/immortalcrab/cfdi/pipeline/IXmlStep.java
+++ fiscal/engine/src/main/java/com/immortalcrab/cfdi/pipeline/IXmlStep.java
@@ -4,7 +4,7 @@ import com.immortalcrab.cfdi.error.FormatError;
 import com.immortalcrab.cfdi.error.StorageError;
 
 @FunctionalInterface
-public interface IXmlStep {
+interface IXmlStep {
 
     public String render(Request cfdiReq, IStorage st) throws FormatError, StorageError;
 }
