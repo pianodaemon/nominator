096c0a62d30c6126496baca4ee011bbd2dff57ed
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-07 13:01:31 -0600

    Update PacRegularStamp.java

--- fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/PacRegularStamp.java
+++ fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/PacRegularStamp.java
@@ -2,7 +2,9 @@ package com.immortalcrab.cfdi.pipeline;
 
 import com.immortalcrab.cfdi.error.FormatError;
 
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class PacRegularStamp implements IStamp<PacRegularRequest, PacRegularResponse> {
 
     @Override
@@ -10,5 +12,10 @@ public class PacRegularStamp implements IStamp<PacRegularRequest, PacRegularResp
 
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    
+
+    public static PacRegularStamp setupWithEnv() {
+
+        // It is still pending to add code here
+        return new PacRegularStamp();
+    }
 }
