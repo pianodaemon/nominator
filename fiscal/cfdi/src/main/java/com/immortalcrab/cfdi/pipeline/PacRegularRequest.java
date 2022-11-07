04a9ade2dcdbed1f3525352601e4add9e511096b
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-07 13:30:09 -0600

    Update PacRegularRequest.java

--- fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/PacRegularRequest.java
+++ fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/PacRegularRequest.java
@@ -1,5 +1,16 @@
 package com.immortalcrab.cfdi.pipeline;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@AllArgsConstructor
+@Getter
 class PacRegularRequest {
+
+    private final @NonNull
+    String xmlStr;
 
 }
