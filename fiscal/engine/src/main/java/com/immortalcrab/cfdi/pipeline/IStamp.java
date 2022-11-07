068fb9cf746a3ce2f5af4d8ffa5f54b6231db0e3
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-07 14:29:08 -0600

    Update IStamp.java

--- fiscal/engine/src/main/java/com/immortalcrab/cfdi/pipeline/IStamp.java
+++ fiscal/engine/src/main/java/com/immortalcrab/cfdi/pipeline/IStamp.java
@@ -3,7 +3,7 @@ package com.immortalcrab.cfdi.pipeline;
 import com.immortalcrab.cfdi.error.FormatError;
 
 @FunctionalInterface
-interface IStamp<V, T> {
+interface IStamp<V extends PacReq, T extends PacRes> {
 
     public T impress(final V target) throws FormatError;
 }
