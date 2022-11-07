27de6ebf66d705739adac21467e87476d2625c26
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-07 13:54:42 -0600

    Update FacturaXml.java

--- fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/FacturaXml.java
+++ fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/FacturaXml.java
@@ -215,12 +215,14 @@ class FacturaXml {
         return sw;
     }
 
-    public static String render(Request cfdiReq, IStamp stamper, IStorage st) throws FormatError, StorageError {
+   public static String render(Request cfdiReq, IStamp<PacRegularRequest, PacRegularResponse> stamper, IStorage st) throws FormatError, StorageError {
 
         FacturaXml ic = new FacturaXml(cfdiReq, st);
 
         StringWriter cfdi = ic.shape();
-
+        PacRegularRequest pacReq = new PacRegularRequest(cfdi.toString());
+        PacRegularResponse pacRes = stamper.impress(pacReq);
+ 
         return "It must be slightly implemented as it was in lola";
     }
 
