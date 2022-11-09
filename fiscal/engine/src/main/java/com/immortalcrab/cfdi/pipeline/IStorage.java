c9bc4d185e8602eade9cde5307772d03653578f7
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-08 21:07:29 -0600

    Update IStorage.java

--- fiscal/engine/src/main/java/com/immortalcrab/cfdi/pipeline/IStorage.java
+++ fiscal/engine/src/main/java/com/immortalcrab/cfdi/pipeline/IStorage.java
@@ -1,13 +1,15 @@
 package com.immortalcrab.cfdi.pipeline;
 
 import com.immortalcrab.cfdi.error.StorageError;
+import java.io.BufferedInputStream;
 import java.io.InputStream;
 
-@FunctionalInterface
 interface IStorage {
 
     public void upload(final String cType,
             final long len,
             final String fileName,
             InputStream inputStream) throws StorageError;
+
+    public BufferedInputStream download(final String fileName) throws StorageError;
 }
