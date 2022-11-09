33d6ed84b872f6c215df081792a57d9501a6040b
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-09 06:25:50 -0600

    Update NominaParser.java

--- nominator/docmaker/src/main/java/com/immortalcrab/nominator/formats/NominaParser.java
+++ nominator/docmaker/src/main/java/com/immortalcrab/nominator/formats/NominaParser.java
@@ -1,10 +1,7 @@
 package com.immortalcrab.nominator.formats;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
@@ -12,30 +9,45 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 class NominaParser {
 
-    private void read(InputStream url){
-        try{
+    private String UUID;
+
+    private void read(InputStream url) {
+        try {
             SAXParserFactory spf = SAXParserFactory.newInstance();
             SAXParser sp = spf.newSAXParser();
-            sp.parse(url, new FormatHandler() );
-        }
-        catch(ParserConfigurationException e){
+            sp.parse(url, new FormatHandler());
+        } catch (ParserConfigurationException e) {
             System.err.println("error de  parseo " + e);
-        }
-        catch(SAXException e2){
+        } catch (SAXException e2) {
             System.err.println(e2);
             System.err.println("error de  sax: " + e2.getStackTrace());
-        }
-        catch (IOException e3) {
-            System.err.println("error de  io: " + e3.getMessage() );
+        } catch (IOException e3) {
+            System.err.println("error de  io: " + e3.getMessage());
         }
     }
 
     private class FormatHandler extends DefaultHandler {
 
         public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
-            
+
+            if (qName.equals("tfd:TimbreFiscalDigital")) {
+
+                for (int i = 0; i < atts.getLength(); i++) {
+
+                    String name = atts.getQName(i);
+                    if (name.equals("UUID")) {
+
+                        setUUID(atts.getValue(i));
+                    }
+                }
+            }
         }
     }
 }
