ba3db40f58e6c65ba4c0ef8979183116c487eda9
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-10 00:35:23 -0600

    Update DocMaker.java

--- nominator/docmaker/src/main/java/com/immortalcrab/nominator/serverless/DocMaker.java
+++ nominator/docmaker/src/main/java/com/immortalcrab/nominator/serverless/DocMaker.java
@@ -1,46 +1,40 @@
 package com.immortalcrab.nominator.serverless;
 
-import java.io.IOException;
+import com.google.gson.Gson;
+
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
-import com.google.gson.Gson;
-
 class DocMaker {
 
-	static final String PDF_MIME_TYPE = "application/pdf";
+    static final String PDF_MIME_TYPE = "application/pdf";
 
-	private Map<String, String> setupHeaders(final String fName) {
+    private Map<String, Object> setupResponse(Map<String, String> headers, final byte[] docEncB64, final String fName) {
 
-		return Map.of(
-			"Content-disposition", String.format("attachment; filename=%s", fName),
-			"Content-Type", PDF_MIME_TYPE,
-			"Accept", PDF_MIME_TYPE
-		);
-	}
+        final String body = new String(docEncB64, StandardCharsets.UTF_8);
 
-	private Map<String, Object> setupResponse(Map<String, String> headers, final byte[] docEncB64) {
+        return Map.of(
+                "statusCode", 200,
+                "isBase64Encoded", true,
+                "body", body,
+                "headers", Map.of(
+                        "Content-disposition", String.format("attachment; filename=%s", fName),
+                        "Content-Type", PDF_MIME_TYPE,
+                        "Accept", PDF_MIME_TYPE
+                )
+        );
+    }
 
-		final String body = new String(docEncB64, StandardCharsets.UTF_8);
+    public void flushBuffer(Map<String, Object> responseMap, OutputStream oStream) {
 
-		return Map.of(
-			"statusCode", 200,
-			"isBase64Encoded", true,
-			"body", body,
-			"headers", headers
-		);
-	}
+        Gson gsonObj = new Gson();
 
-	public void flushBuffer(Map<String, Object> responseMap, OutputStream oStream) throws IOException {
+        String responseJSON = gsonObj.toJson(responseMap);
 
-		Gson gsonObj = new Gson();
-
-		String responseJSON = gsonObj.toJson(responseMap);
-
-		OutputStreamWriter writer = new OutputStreamWriter(oStream, StandardCharsets.UTF_8.name());
-		writer.write(responseJSON);
-		writer.close();
-	}
+        OutputStreamWriter writer = new OutputStreamWriter(oStream, StandardCharsets.UTF_8.name());
+        writer.write(responseJSON);
+        writer.close();
+    }
 }
