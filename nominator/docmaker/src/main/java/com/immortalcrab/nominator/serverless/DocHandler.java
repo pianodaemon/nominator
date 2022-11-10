256158ec8efc731d69d66b4cacbf713145d87c50
Edwin Pĺauchu <edwin.plauchu@wunderkind.co>
2022-11-09 19:42:02 -0600

    doc_handler_skeleton

---
+++ nominator/docmaker/src/main/java/com/immortalcrab/nominator/serverless/DocHandler.java
@@ -0,0 +1,14 @@
+package com.immortalcrab.nominator.serverless;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+
+public class DocHandler implements RequestStreamHandler {
+
+    public void handleRequest(InputStream iStream, OutputStream oStream, Context ctx) {
+
+    }
+}
