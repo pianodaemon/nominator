7ca1ce2d409979cccb196ba8b831ef5165584d3e
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-11 13:01:06 -0600

    Create ProducerTest.java

---
+++ fiscal/cfdi/src/test/java/com/immortalcrab/cfdi/pipeline/ProducerTest.java
@@ -0,0 +1,40 @@
+package com.immortalcrab.cfdi.pipeline;
+
+import com.immortalcrab.cfdi.error.StorageError;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+public class ProducerTest {
+
+    FakeBucketService _fbs;
+    FakeStorage _storage;
+    FakeStamp _stamper;
+
+    @BeforeEach
+    void setup() throws StorageError {
+
+        _fbs = new FakeBucketService();
+        _storage = new FakeStorage(_fbs.engage(), "fake");
+    }
+
+    @AfterEach
+    void teardown() {
+
+        _fbs.shutdown();
+    }
+
+    @Test
+    void verifyProduction() {
+
+        try {
+
+            new Producer(_stamper, _storage);
+
+        } catch (StorageError ex) {
+            assertNotNull(ex);
+        }
+    }
+}
