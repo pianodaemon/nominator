ea5718783615db654751b926ddd6284bc1714a4f
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-11 16:51:27 -0600

    Update ProducerTest.java

--- fiscal/cfdi/src/test/java/com/immortalcrab/cfdi/pipeline/ProducerTest.java
+++ fiscal/cfdi/src/test/java/com/immortalcrab/cfdi/pipeline/ProducerTest.java
@@ -1,6 +1,9 @@
 package com.immortalcrab.cfdi.pipeline;
 
+import com.amazonaws.services.s3.AmazonS3;
 import com.immortalcrab.cfdi.error.StorageError;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -8,33 +11,42 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 
 public class ProducerTest {
-
+    
+    static final String DEFAULT_BUCKET = "default_bucket";
+    
     FakeBucketService _fbs;
     FakeStorage _storage;
     FakeStamp _stamper;
-
+    
     @BeforeEach
     void setup() throws StorageError {
-
+        
         _fbs = new FakeBucketService();
-        _storage = new FakeStorage(_fbs.engage(), "fake");
+        _storage = new FakeStorage(_fbs.engage(), DEFAULT_BUCKET);
     }
-
+    
     @AfterEach
     void teardown() {
-
+        
         _fbs.shutdown();
     }
-
+    
     @Test
     void verifyProduction() {
-
-       /* try {
-
+        
+        try {
+            
+            AmazonS3 client = _fbs.getClient().orElseThrow(() -> new StorageError("aws client was never initialized"));
+            client.createBucket(DEFAULT_BUCKET);
+            /* try {
+            
             new Producer(_stamper, _storage);
-
-        } catch (StorageError ex) {
+            
+            } catch (StorageError ex) {
             assertNotNull(ex);
-        }*/
+            }*/
+        } catch (StorageError ex) {
+            Logger.getLogger(ProducerTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
