f8bec67f5bb0214702b3ffa367cd7b5ba0b9facd
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-11 20:36:47 -0600

    Update FakeStamp.java

--- fiscal/cfdi/src/test/java/com/immortalcrab/cfdi/pipeline/FakeStamp.java
+++ fiscal/cfdi/src/test/java/com/immortalcrab/cfdi/pipeline/FakeStamp.java
@@ -1,14 +1,39 @@
 package com.immortalcrab.cfdi.pipeline;
 
 import com.immortalcrab.cfdi.error.FormatError;
+import com.immortalcrab.cfdi.pipeline.FakeStamp.PacReqChild;
+import com.immortalcrab.cfdi.pipeline.FakeStamp.PacResChild;
+import java.io.StringWriter;
 
-public class FakeStamp implements IStamp {
-
-    public FakeStamp() {
-    }
+public class FakeStamp implements IStamp<PacReqChild, PacResChild> {
 
     @Override
-    public PacRes impress(PacReq target) throws FormatError {
+    public PacResChild impress(PacReqChild target) throws FormatError {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public PacReqChild createPacReq(String xmlStr, String targetUri) {
+
+        return new PacReqChild(xmlStr, targetUri);
+    }
+
+    public class PacReqChild extends PacReq {
+
+        public PacReqChild(String xmlStr, String targetUri) {
+            super(xmlStr, targetUri);
+        }
+
+    }
+
+    public class PacResChild extends PacRes {
+
+        StringWriter getContent() {
+            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        }
+
+        String getID() {
+            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        }
+
     }
 }
