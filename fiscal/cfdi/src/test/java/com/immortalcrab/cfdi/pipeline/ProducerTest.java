b33de7482a1217d5018e1b4286644fa4c684c1c6
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-11 21:03:39 -0600

    Update ProducerTest.java

--- fiscal/cfdi/src/test/java/com/immortalcrab/cfdi/pipeline/ProducerTest.java
+++ fiscal/cfdi/src/test/java/com/immortalcrab/cfdi/pipeline/ProducerTest.java
@@ -1,13 +1,29 @@
 package com.immortalcrab.cfdi.pipeline;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.google.common.collect.ImmutableMap;
+import com.immortalcrab.cfdi.error.DecodeError;
+import com.immortalcrab.cfdi.error.FormatError;
+import com.immortalcrab.cfdi.error.PipelineError;
+import com.immortalcrab.cfdi.error.RequestError;
 import com.immortalcrab.cfdi.error.StorageError;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.AfterEach;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import org.javatuples.Pair;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ProducerTest {
 
     static final String DEFAULT_BUCKET = "datalake-cfdi-subscriber";
@@ -15,12 +31,20 @@ public class ProducerTest {
     FakeBucketService _fbs;
     IStorage _storage;
     IStamp _stamper;
+    private ClassLoader _cloader;
+
+    @BeforeAll
+    public void setUpClass() {
+
+        _cloader = getClass().getClassLoader();
+    }
 
     @BeforeEach
     void setup() throws StorageError {
 
         _fbs = new FakeBucketService();
         _storage = new S3BucketStorage(_fbs.engage(), DEFAULT_BUCKET);
+        _stamper = new FakeStamp();
     }
 
     @AfterEach
@@ -31,16 +55,26 @@ public class ProducerTest {
 
     @Test
     void verifyProduction() {
-
+/*
         try {
 
             AmazonS3 client = _fbs.getClient().orElseThrow(() -> new StorageError("aws client was never initialized"));
             client.createBucket(DEFAULT_BUCKET);
-            
-            //new Producer(_stamper, _storage);
 
-        } catch (StorageError ex) {
+            InputStream is = _cloader.getResourceAsStream("jsonreqs/nominareq.json");
+            InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
+
+            Pair<IDecodeStep, IXmlStep> pair = new Pair<>(FakeRequest::render, FakeXml::render);
+            Producer producer = new Producer(_stamper, _storage, ImmutableMap.of("fake", pair));
+
+            producer.doIssue("nom", isr);
+
+            BufferedInputStream buff = _storage.download(DEFAULT_BUCKET);
+            assertTrue(buff.readAllBytes().length > 0);
+
+        } catch (StorageError | PipelineError | RequestError | FormatError | DecodeError | IOException ex) {
             assertNotNull(ex);
-        }
+        }*/
     }
+
 }
