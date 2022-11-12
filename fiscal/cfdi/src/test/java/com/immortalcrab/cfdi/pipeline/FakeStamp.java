c737942eb3c109203d594783c18d0c29d4b4d2d4
Edwin Pĺauchu <edwin.plauchu@wunderkind.co>
2022-11-11 20:46:25 -0600

    Revert "Update FakeStamp.java"
    This reverts commit f8bec67f5bb0214702b3ffa367cd7b5ba0b9facd.

--- fiscal/cfdi/src/test/java/com/immortalcrab/cfdi/pipeline/FakeStamp.java
+++ fiscal/cfdi/src/test/java/com/immortalcrab/cfdi/pipeline/FakeStamp.java
@@ -1,39 +1,14 @@
 package com.immortalcrab.cfdi.pipeline;
 
 import com.immortalcrab.cfdi.error.FormatError;
-import com.immortalcrab.cfdi.pipeline.FakeStamp.PacReqChild;
-import com.immortalcrab.cfdi.pipeline.FakeStamp.PacResChild;
-import java.io.StringWriter;
 
-public class FakeStamp implements IStamp<PacReqChild, PacResChild> {
+public class FakeStamp implements IStamp {
+
+    public FakeStamp() {
+    }
 
     @Override
-    public PacResChild impress(PacReqChild target) throws FormatError {
+    public PacRes impress(PacReq target) throws FormatError {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public PacReqChild createPacReq(String xmlStr, String targetUri) {
-
-        return new PacReqChild(xmlStr, targetUri);
-    }
-
-    public class PacReqChild extends PacReq {
-
-        public PacReqChild(String xmlStr, String targetUri) {
-            super(xmlStr, targetUri);
-        }
-
-    }
-
-    public class PacResChild extends PacRes {
-
-        StringWriter getContent() {
-            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-        }
-
-        String getID() {
-            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-        }
-
     }
 }
