6ef659acbb4a28bd57f8e2b9f369791cf95a5c8c
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-13 16:10:47 -0600

    Update NominaRequestDTO.java

--- fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/NominaRequestDTO.java
+++ fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/NominaRequestDTO.java
@@ -19,6 +19,7 @@ import lombok.extern.log4j.Log4j;
 class NominaRequestDTO extends JsonRequest {
 
     public static final String VERSION = "4.0";
+    public static final String VERSION_NOMINA = "1.2";
     public static final String TIPO_COMPROBANTE = "N";
 
     DocPrincipalAttributes _docAttribs;
