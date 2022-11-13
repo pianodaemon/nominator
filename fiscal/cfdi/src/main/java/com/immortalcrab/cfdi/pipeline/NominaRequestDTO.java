117f7229befc56b4ea7f4502ae74149e236a02c8
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-13 09:56:47 -0600

    Update NominaRequestDTO.java

--- fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/NominaRequestDTO.java
+++ fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/NominaRequestDTO.java
@@ -68,6 +68,7 @@ class NominaRequestDTO extends JsonRequest {
             Optional<Object> fecha = LegoAssembler.obtainObjFromKey(this.getDs(), "fecha");
             Optional<Object> moneda = LegoAssembler.obtainObjFromKey(this.getDs(), "moneda");
             Optional<Object> exportacion = LegoAssembler.obtainObjFromKey(this.getDs(), "exportacion");
+            Optional<Object> metodoPago = LegoAssembler.obtainObjFromKey(this.getDs(), "metodo_pago");
 
             _docAttribs.setSerie((String) serie.orElseThrow());
             _docAttribs.setFolio((String) folio.orElseThrow());
@@ -180,6 +181,7 @@ class NominaRequestDTO extends JsonRequest {
         private String folio;
         private String moneda;
         private String exportacion;
+        private String metodoPago;
     }
 
     @NoArgsConstructor
