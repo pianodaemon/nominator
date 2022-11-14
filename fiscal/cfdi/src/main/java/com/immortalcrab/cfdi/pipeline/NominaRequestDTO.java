1606a3d9ca578333d288fae560f26d151579cd78
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-14 15:25:24 -0600

    Update NominaRequestDTO.java

--- fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/NominaRequestDTO.java
+++ fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/NominaRequestDTO.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 
 import lombok.Getter;
