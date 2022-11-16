c8fd93cd9608ff59c206b8cffe940a4277b67b8e
Edwin Pĺauchu <pianodaemon@gmail.com>
2022-11-15 20:08:53 -0600

    broken3

--- fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/Producer.java
+++ fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/Producer.java
@@ -1,20 +1,27 @@
 package com.immortalcrab.cfdi.pipeline;
 
-import com.google.common.collect.ImmutableMap;
 import com.immortalcrab.cfdi.error.DecodeError;
 import com.immortalcrab.cfdi.error.FormatError;
 import com.immortalcrab.cfdi.error.PipelineError;
 import com.immortalcrab.cfdi.error.RequestError;
 import com.immortalcrab.cfdi.error.StorageError;
+import java.io.ByteArrayInputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 import org.javatuples.Pair;
 
 public class Producer extends Pipeline implements IIssuer {
 
     public Producer() throws StorageError {
 
-        this(PacRegularStamp.setupWithEnv(),
-                new S3BucketStorage(S3ClientHelper.setupWithEnv(), System.getenv("BUCKET_TARGET")));
+        this(PacRegularStamp.setupWithEnv());
+    }
+
+    public Producer(final IStamp stamper) throws StorageError {
+
+        this(stamper,
+                new S3BucketStorage(S3ClientHelper.setupWithEnv(), System.getenv("BUCKET_PERSISTANCE_TARGET")));
     }
 
     Producer(final IStamp stamper, final IStorage storage) throws StorageError {
@@ -22,13 +29,13 @@ public class Producer extends Pipeline implements IIssuer {
         this(
                 stamper,
                 storage,
-                ImmutableMap.of(
+                Map.of(
                         "fac", new Pair<>(reader -> new FacturaRequestDTO(reader), FacturaXml::render),
                         "nom", new Pair<>(reader -> new NominaRequestDTO(reader), NominaXml::render))
         );
     }
 
-    Producer(final IStamp stamper, final IStorage storage, ImmutableMap<String, Pair<IDecodeStep, IXmlStep>> scenarios) throws StorageError {
+    Producer(final IStamp stamper, final IStorage storage, Map<String, Pair<IDecodeStep, IXmlStep>> scenarios) throws StorageError {
         super(stamper, storage, scenarios);
     }
 
@@ -37,5 +44,14 @@ public class Producer extends Pipeline implements IIssuer {
             throws DecodeError, RequestError, PipelineError, StorageError, FormatError {
 
         return this.issue(kind, isr);
+    }
+
+    @Override
+    protected void saveOnPersistance(IStorage st, PacRes pacResult) throws StorageError {
+
+        final String fileName = String.format("%s/%s.%s", st.getTargetName(), pacResult.getContent().getName(), ".xml");
+        byte[] in = pacResult.getContent().getBuffer().toString().getBytes(StandardCharsets.UTF_8);
+
+        st.upload("text/xml", in.length, fileName, new ByteArrayInputStream(in));
     }
 }
