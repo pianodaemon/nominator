bfab1c18fa934a2c261671385bb4b515002d39ab
Edwin Pĺauchu <pianodaemon@gmail.com>
2022-11-15 20:14:36 -0600

    splat

--- fiscal/cfdi/src/test/java/com/immortalcrab/cfdi/pipeline/FakeStamp.java
+++ fiscal/cfdi/src/test/java/com/immortalcrab/cfdi/pipeline/FakeStamp.java
@@ -3,22 +3,24 @@ package com.immortalcrab.cfdi.pipeline;
 import com.immortalcrab.cfdi.error.FormatError;
 import java.io.StringWriter;
 
-public class FakeStamp implements IStamp<PacReqChild, PacResChild> {
+public class FakeStamp implements IStamp<PacReqChild, PacRes> {
 
     static String EVERYDAY_UUID = "43ebcda4-6339-4e31-8d25-2aa28ee50e54";
 
     @Override
-    public PacResChild impress(PacReqChild target) throws FormatError {
+    public PacRes impress(PacReqChild target) throws FormatError {
 
         StringWriter sw = new StringWriter();
+
+        // We fake the impressing step
         sw.write(target.getXmlStr().replace("to get", "is"));
-        PacResChild res = new PacResChild(sw, EVERYDAY_UUID);
-        return res;
+        PacRes.Content content = new PacRes.Content(sw.getBuffer(), target.getTrailAlias(), EVERYDAY_UUID);
+        return new PacRes(0, content);
     }
 
-    public PacReqChild createPacReq(String xmlStr, String targetUri) {
+    public PacReqChild createPacReq(String xmlStr, String targetUri, String trailAlias) {
 
-        return new PacReqChild(xmlStr, targetUri);
+        return new PacReqChild(xmlStr, targetUri, trailAlias);
     }
 
 }
