29a5749d97c782cef55a01de167372f020db434e
Edwin Pĺauchu <pianodaemon@gmail.com>
2022-11-17 09:48:19 -0600

    ver

--- dev-tools/deployment/verify-infra-stack.sh
+++ dev-tools/deployment/verify-infra-stack.sh
@@ -1,1 +1,11 @@
-awslocal s3 ls
+#!/bin/bash -x
+
+SUBSCRIPTOR="dummy"
+
+# Verification for expected buckets
+{
+    expected_buckets=(cfdi-datalake cfdi-datares)
+    for b in "${expected_buckets[@]}"; do
+        awslocal s3 ls s3://${b}-${SUBSCRIPTOR}
+    done
+}
