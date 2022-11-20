461fc3b8f13a80492f0e5988ab67055d219a2302
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-20 13:09:40 -0600

    Update test_event_bus.py

--- dev-tools/deployment/test_event_bus.py
+++ dev-tools/deployment/test_event_bus.py
@@ -1,14 +1,15 @@
 import boto3
 import json
 
+subscriptor="medica"
 client = boto3.client('events')
-_bus = 'cfdi-eventbus-dummy'
+_bus = 'cfdi-eventbus-{}'.format(subscriptor)
 payload=dict([("username", "j4nusx"), ("city", "SJO"), ("age", "42")])
 detailJsonString = json.dumps(payload)
 
 entry = {
-    'Source':'cfdi-issuer-client',
-    'DetailType':'user-preferences',
+    'Source':'{}'.format(subscriptor),
+    'DetailType':'cfdi-issuer-client',
     'Detail': '{}'.format(detailJsonString),
     'EventBusName': '{}'.format(_bus),
 }
