9abde30e5ed40e7b4b9293791d1c99e0efbfb7ea
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-23 07:11:08 -0600

    Create Payload.java

---
+++ fiscal/factory/src/main/java/com/immortalcrab/cfdi/serverless/Payload.java
@@ -0,0 +1,58 @@
+package com.immortalcrab.cfdi.serverless;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Payload implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @JsonProperty("kind")
+    private String kind = null;
+
+    @JsonProperty("req")
+    private String req = null;
+
+    @Override
+    public boolean equals(java.lang.Object o) {
+
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Payload p = (Payload) o;
+        return Objects.equals(this.kind, p.kind)
+                && Objects.equals(this.req, p.req);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(kind, req);
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("class Payload {\n");
+
+        sb.append("    kind: ").append(toIndentedString(kind)).append("\n");
+        sb.append("    req: ").append(toIndentedString(req)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    private String toIndentedString(java.lang.Object o) {
+
+        if (o == null) {
+            return "null";
+        }
+
+        return o.toString().replace("\n", "\n    ");
+    }
+}
