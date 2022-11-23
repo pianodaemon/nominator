875967e8b9c1ed9176a271e70784a02dccdd4b53
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-23 07:10:26 -0600

    Update Marshaller.java

--- fiscal/factory/src/main/java/com/immortalcrab/cfdi/serverless/Marshaller.java
+++ fiscal/factory/src/main/java/com/immortalcrab/cfdi/serverless/Marshaller.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class Marshaller {
+class Marshaller {
 
     private static final ObjectMapper mapper = createObjectMapper();
 
