e619624f838704afd142faacde671abd22c3e284
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-23 15:00:48 -0600

    Update AWSEvent.java

--- fiscal/factory/src/main/java/com/immortalcrab/cfdi/serverless/AWSEvent.java
+++ fiscal/factory/src/main/java/com/immortalcrab/cfdi/serverless/AWSEvent.java
@@ -2,6 +2,9 @@ package com.immortalcrab.cfdi.serverless;
 
 import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import java.io.IOException;
 
 import java.util.Date;
@@ -212,5 +215,22 @@ class AWSEvent<T> {
 
     public static <T> AWSEvent<T> unmarshalEvent(String input, Class<T> type) throws IOException {
         return Marshaller.unmarshalEvent(input, type);
+    }
+
+    private static class Marshaller {
+
+        private static final ObjectMapper mapper = createObjectMapper();
+
+        public static <T> AWSEvent<T> unmarshalEvent(String input, Class<T> type) throws IOException {
+
+            final TypeFactory typeFactory = mapper.getTypeFactory();
+            return mapper.readValue(input, typeFactory.constructParametricType(AWSEvent.class, type));
+        }
+
+        private static ObjectMapper createObjectMapper() {
+            return new ObjectMapper()
+                    .findAndRegisterModules()
+                    .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        }
     }
 }
