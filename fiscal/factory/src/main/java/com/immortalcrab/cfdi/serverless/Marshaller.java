cd81cbc5d003864137a803e3e560d7de45d74d73
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-23 14:52:00 -0600

    Update Marshaller.java

--- fiscal/factory/src/main/java/com/immortalcrab/cfdi/serverless/Marshaller.java
+++ fiscal/factory/src/main/java/com/immortalcrab/cfdi/serverless/Marshaller.java
@@ -5,20 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 class Marshaller {
 
     private static final ObjectMapper mapper = createObjectMapper();
-
-    public static <T> void marshal(OutputStream output, T value) throws IOException {
-        mapper.writeValue(output, value);
-    }
-
-    public static <T> T unmarshal(InputStream input, Class<T> type) throws IOException {
-        return mapper.readValue(input, type);
-    }
 
     public static <T> AWSEvent<T> unmarshalEvent(String input, Class<T> type) throws IOException {
 
