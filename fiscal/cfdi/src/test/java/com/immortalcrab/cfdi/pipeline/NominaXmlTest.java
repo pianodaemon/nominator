4ceee33046cb7b49698bf06901a221d5082fd763
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-24 17:00:41 -0600

    Update NominaXmlTest.java

--- fiscal/cfdi/src/test/java/com/immortalcrab/cfdi/pipeline/NominaXmlTest.java
+++ fiscal/cfdi/src/test/java/com/immortalcrab/cfdi/pipeline/NominaXmlTest.java
@@ -1,8 +1,5 @@
 package com.immortalcrab.cfdi.pipeline;
 
-import com.immortalcrab.cfdi.error.DecodeError;
-import com.immortalcrab.cfdi.error.FormatError;
-import com.immortalcrab.cfdi.error.RequestError;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -10,14 +7,13 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestInstance(Lifecycle.PER_CLASS)
 public class NominaXmlTest {
+
     private ClassLoader _cloader;
 
     @BeforeAll
@@ -30,19 +26,21 @@ public class NominaXmlTest {
     public void loadReqFromReader() {
 
         try {
-            InputStream xmlIs = _cloader.getResourceAsStream("xmlsamples/nomina.xml");
-            InputStreamReader xmlReader = new InputStreamReader(xmlIs, StandardCharsets.UTF_8);
-            String xml = xmlReader.toString();
+            InputStream isTesting = _cloader.getResourceAsStream("xmlsamples/nomina.xml");
+            String nominaTesting = new String(isTesting.readAllBytes(), StandardCharsets.UTF_8);
 
             InputStream is = _cloader.getResourceAsStream("jsonreqs/nominareq.json");
             InputStreamReader reader = new InputStreamReader(is, StandardCharsets.UTF_8);
-            NominaXml nomina = new NominaXml(new NominaRequestDTO(reader));
+            NominaXml nominaGenerated = new NominaXml(new NominaRequestDTO(reader));
 
-	    assertTrue(true);
-            //assertTrue(xml.equals(nomina.toString()));
+            char[] csc0 =nominaGenerated.toString().replace("\n", "").replace("\r", "").toCharArray();
+            char[] csc1 = nominaTesting.toString().replace("\n", "").replace("\r", "").toCharArray();
+            for (int i = 0; i < csc0.length; i++) {
+                assertTrue(csc0[i]== csc1[i]);
+            }
 
-        } catch (RequestError | DecodeError | FormatError ex) {
-            assertNotNull(ex);
+        } catch (Exception ex) {
+            assertNull(ex);
         }
     }
 }
