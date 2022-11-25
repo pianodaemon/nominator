9626343242498a161d3b16451835e18396aa6920
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-25 17:02:12 -0600

    Update Request.java

--- fiscal/engine/src/main/java/com/immortalcrab/cfdi/pipeline/Request.java
+++ fiscal/engine/src/main/java/com/immortalcrab/cfdi/pipeline/Request.java
@@ -1,40 +1,17 @@
 package com.immortalcrab.cfdi.pipeline;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.immortalcrab.cfdi.error.DecodeError;
 import com.immortalcrab.cfdi.error.RequestError;
-import java.io.IOException;
+import com.immortalcrab.cfdi.utils.JsonToMapHelper;
 import java.io.InputStreamReader;
-import java.util.Map;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Getter
-@AllArgsConstructor
-public class Request {
-
-    protected final @NonNull
-    Map<String, Object> ds;
+public class Request extends JsonToMapHelper{
 
     protected Request(InputStreamReader reader) throws RequestError, DecodeError {
-        this(Request.readFromJson(reader));
-    }
-
-    private static Map<String, Object> readFromJson(InputStreamReader reader) throws DecodeError {
-        ObjectMapper mapper = new ObjectMapper();
-
-        TypeReference<Map<String, Object>> tr = new TypeReference<Map<String, Object>>() {
-        };
-
-        try {
-            return (mapper.readValue(reader, tr));
-        } catch (IOException ex) {
-            log.warn(ex.getMessage());
-            throw new DecodeError("Issue found when reading json buffer", ex);
-        }
+        super(JsonToMapHelper.readFromReader(reader));
     }
 }
