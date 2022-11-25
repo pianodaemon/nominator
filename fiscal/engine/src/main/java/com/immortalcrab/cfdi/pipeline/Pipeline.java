3955953a9e92280cf2c5a115d51b735e5865be6c
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-25 09:43:44 -0600

    Payload interface (#66)

--- fiscal/engine/src/main/java/com/immortalcrab/cfdi/pipeline/Pipeline.java
+++ fiscal/engine/src/main/java/com/immortalcrab/cfdi/pipeline/Pipeline.java
@@ -5,7 +5,9 @@ import com.immortalcrab.cfdi.error.FormatError;
 import com.immortalcrab.cfdi.error.PipelineError;
 import com.immortalcrab.cfdi.error.RequestError;
 import com.immortalcrab.cfdi.error.StorageError;
+import java.io.BufferedInputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.Optional;
 import org.javatuples.Pair;
@@ -32,7 +34,7 @@ abstract class Pipeline {
     private final @NonNull
     Map<String, Pair<IDecodeStep, IXmlStep>> scenarios;
 
-    public String issue(final String kind, InputStreamReader isr)
+    private String engage(final String kind, InputStreamReader isr)
             throws DecodeError, RequestError, PipelineError, StorageError, FormatError {
 
         Optional<Pair<IDecodeStep, IXmlStep>> stages = Optional.ofNullable(this.getScenarios().get(kind));
@@ -55,6 +57,21 @@ abstract class Pipeline {
         saveOnPersistance(this.getStorage(), pacResult);
 
         return pacResult.getContent().getId();
+    }
+
+    protected String doIssue(final IPayload payload)
+            throws DecodeError, RequestError, PipelineError, StorageError, FormatError {
+
+        BufferedInputStream bf = this.getStorage().download(payload.getReq());
+        InputStreamReader isr = new InputStreamReader(bf, StandardCharsets.UTF_8);
+
+        return this.engage(payload.getKind(), isr);
+    }
+
+    protected String doIssue(final String kind, InputStreamReader isr)
+            throws DecodeError, RequestError, PipelineError, StorageError, FormatError {
+
+        return this.engage(kind, isr);
     }
 
     abstract protected void saveOnPersistance(IStorage st, PacRes pacResult) throws StorageError;
