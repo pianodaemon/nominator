3f7b013894471e86fee69a0fc1e723572235b8fa
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-27 14:50:30 -0600

    Update IDecodeStep.java

--- fiscal/engine/src/main/java/com/immortalcrab/cfdi/pipeline/IDecodeStep.java
+++ fiscal/engine/src/main/java/com/immortalcrab/cfdi/pipeline/IDecodeStep.java
@@ -5,7 +5,7 @@ import com.immortalcrab.cfdi.error.RequestError;
 import java.io.InputStreamReader;
 
 @FunctionalInterface
-interface IDecodeStep {
+interface IDecodeStep<R extends Request> {
 
-    public Request render(InputStreamReader inReader) throws RequestError, DecodeError;
+    public R render(InputStreamReader ir) throws RequestError, DecodeError;
 }
