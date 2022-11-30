248d945b41863814beb52e21011966ec5b559a7f
Edwin Plauchu <pianodaemon@gmail.com>
2022-11-30 12:58:55 -0600

    Update IStorage.java

--- fiscal/engine/src/main/java/com/immortalcrab/cfdi/pipeline/IStorage.java
+++ fiscal/engine/src/main/java/com/immortalcrab/cfdi/pipeline/IStorage.java
@@ -3,6 +3,8 @@ package com.immortalcrab.cfdi.pipeline;
 import com.immortalcrab.cfdi.error.StorageError;
 import java.io.BufferedInputStream;
 import java.io.InputStream;
+import java.util.Map;
+import java.util.Optional;
 
 interface IStorage {
 
@@ -14,4 +16,17 @@ interface IStorage {
     public BufferedInputStream download(final String fileName) throws StorageError;
 
     public String getTargetName() throws StorageError;
+
+    Optional<Map<String, String>> getPathPrefixes();
+
+    default Optional<String> getPathPrefix(final String label) {
+
+        if (getPathPrefixes().isPresent()) {
+
+            Map<String, String> prefixes = getPathPrefixes().get();
+            return Optional.ofNullable(prefixes.get(label));
+        }
+
+        return Optional.ofNullable(null);
+    }
 }
