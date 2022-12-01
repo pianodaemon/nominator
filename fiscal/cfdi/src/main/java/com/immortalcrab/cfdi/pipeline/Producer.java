1a2000f460aca8616f0f4eed687559c9aa884e41
Edwin Plauchu <pianodaemon@gmail.com>
2022-12-01 17:29:42 -0600

    Update Producer.java

--- fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/Producer.java
+++ fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/Producer.java
@@ -108,14 +108,14 @@ public class Producer extends Pipeline {
         public static <R extends Request> PacRes fac(R req, IStamp<PacRegularRequest, PacRes> stamper,
                 BufferedInputStream certificate, BufferedInputStream signerKey) throws FormatError, StorageError {
 
-            FacturaXml ic = new FacturaXml((FacturaRequestDTO) req);
+            FacturaXml ic = new FacturaXml((FacturaRequestDTO) req, certificate, signerKey);
             return stamper.impress(new PacRegularRequest(ic.toString()));
         }
 
         public static <R extends Request> PacRes nom(R req, IStamp<PacRegularRequest, PacRes> stamper,
                 BufferedInputStream certificate, BufferedInputStream signerKey) throws FormatError, StorageError {
 
-            NominaXml ic = new NominaXml((NominaRequestDTO) req);
+            NominaXml ic = new NominaXml((NominaRequestDTO) req, certificate, signerKey);
             return stamper.impress(new PacRegularRequest(ic.toString()));
         }
     }
