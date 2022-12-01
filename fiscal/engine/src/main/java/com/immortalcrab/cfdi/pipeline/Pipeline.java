a86f32013497dfeb9091b675afed5500f322ed99
Edwin Plauchu <pianodaemon@gmail.com>
2022-12-01 08:34:42 -0600

    Update Pipeline.java

--- fiscal/engine/src/main/java/com/immortalcrab/cfdi/pipeline/Pipeline.java
+++ fiscal/engine/src/main/java/com/immortalcrab/cfdi/pipeline/Pipeline.java
@@ -5,6 +5,8 @@ import com.immortalcrab.cfdi.error.FormatError;
 import com.immortalcrab.cfdi.error.PipelineError;
 import com.immortalcrab.cfdi.error.RequestError;
 import com.immortalcrab.cfdi.error.StorageError;
+import java.io.BufferedInputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Map;
 import java.util.Optional;
@@ -70,6 +72,37 @@ public abstract class Pipeline {
     public interface Pickard {
 
         String route(final String kind, InputStreamReader isr) throws DecodeError, RequestError, PipelineError, StorageError, FormatError;
+    }
+
+    @FunctionalInterface
+    public interface IPayload {
+
+        String getReq();
+    }
+
+    interface IStorage {
+
+        public void upload(final String cType,
+                final long len,
+                final String fileName,
+                InputStream inputStream) throws StorageError;
+
+        public BufferedInputStream download(final String fileName) throws StorageError;
+
+        public String getTargetName() throws StorageError;
+
+        Optional<Map<String, String>> getPathPrefixes();
+
+        default Optional<String> getPathPrefix(final String label) {
+
+            if (getPathPrefixes().isPresent()) {
+
+                Map<String, String> prefixes = getPathPrefixes().get();
+                return Optional.ofNullable(prefixes.get(label));
+            }
+
+            return Optional.ofNullable(null);
+        }
     }
 
     @FunctionalInterface
