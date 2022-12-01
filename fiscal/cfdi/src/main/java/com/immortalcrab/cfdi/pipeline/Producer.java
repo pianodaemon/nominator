c7eb167fb7e1889b6958753e9f4a2636b6079fc7
Edwin Plauchu <pianodaemon@gmail.com>
2022-12-01 16:10:29 -0600

    Update Producer.java

--- fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/Producer.java
+++ fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/Producer.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 import lombok.extern.log4j.Log4j2;
 import org.javatuples.Pair;
