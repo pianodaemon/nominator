5d7db42379fda918bbe7b65b37aaa9db4f1d76c7
Edwin Plauchu <pianodaemon@gmail.com>
2022-12-02 15:52:39 -0600

    Update S3ClientHelper.java

--- fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/S3ClientHelper.java
+++ fiscal/cfdi/src/main/java/com/immortalcrab/cfdi/pipeline/S3ClientHelper.java
@@ -11,19 +11,19 @@ import java.util.Optional;
 
 class S3ClientHelper {
 
-    static public AmazonS3 setupWithEnv() throws StorageError {
+    public static AmazonS3 setupWithEnv() throws StorageError {
 
         Optional<String> region = Optional.ofNullable(System.getenv("AWS_REGION"));
         Optional<String> key = Optional.ofNullable(System.getenv("AWS_ACCESS_KEY_ID"));
         Optional<String> secret = Optional.ofNullable(System.getenv("AWS_SECRET_ACCESS_KEY"));
 
-        region.orElseThrow(() -> new StorageError("aws region was not fed"));
-        key.orElseThrow(() -> new StorageError("aws key was not fed"));
-        secret.orElseThrow(() -> new StorageError("aws secret was not fed"));
+        AWSCredentials awsCredentials = new BasicAWSCredentials(
+                key.orElseThrow(() -> new StorageError("aws key was not fed")),
+                secret.orElseThrow(() -> new StorageError("aws secret was not fed")));
 
-        AWSCredentials awsCredentials = new BasicAWSCredentials(key.get(), secret.get());
-
-        return AmazonS3ClientBuilder.standard().withRegion(region.get()).withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).build();
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(region.orElseThrow(() -> new StorageError("aws region was not fed")))
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).build();
     }
 
     private S3ClientHelper() {
