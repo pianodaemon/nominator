d0a7faf1d04e15e2841f07b9499e0f99df747821
Edwin Pĺauchu <pianodaemon@gmail.com>
2023-04-03 13:01:54 -0500

    resource_xslt

--- cfdiengine/elemental/src/main/java/com/immortalcrab/cfdi/xml/FacturaXml.java
+++ cfdiengine/elemental/src/main/java/com/immortalcrab/cfdi/xml/FacturaXml.java
@@ -2,8 +2,9 @@ package com.immortalcrab.cfdi.xml;
 
 import com.immortalcrab.cfdi.dtos.FacturaRequestDTO;
 import com.immortalcrab.cfdi.errors.EngineError;
-import com.immortalcrab.cfdi.errors.ErrorCodes;
-
+import com.immortalcrab.cfdi.toolbox.IToolbox;
+import lombok.NonNull;
+import lombok.extern.log4j.Log4j2;
 import mx.gob.sat.cfd._4.Comprobante;
 import mx.gob.sat.cfd._4.ObjectFactory;
 import mx.gob.sat.sitio_internet.cfd.catalogos.*;
@@ -11,14 +12,11 @@ import mx.gob.sat.sitio_internet.cfd.catalogos.*;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
-import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.transform.stream.StreamSource;
+import java.io.*;
 import java.util.List;
-import java.io.BufferedInputStream;
-import java.io.StringWriter;
-
-import lombok.NonNull;
-import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class FacturaXml {
@@ -28,11 +26,18 @@ public class FacturaXml {
 
     private final StringWriter sw;
 
+    private ClassLoader _cloader;
+
+    public void setUpClass() {
+
+        _cloader = getClass().getClassLoader();
+    }
+
     public FacturaXml(FacturaRequestDTO req,
             BufferedInputStream certificate, BufferedInputStream signerKey, final String certificateNo) throws EngineError {
 
         this.req = req;
-        this.sw = shape();
+        this.sw = shape(certificateNo, certificate, signerKey);
     }
 
     @Override
@@ -40,9 +45,9 @@ public class FacturaXml {
         return sw.toString();
     }
 
-    private StringWriter shape() throws EngineError {
+    private StringWriter shape(String certificateNo, BufferedInputStream certificate, BufferedInputStream signerKey) throws EngineError {
 
-        StringWriter writer = new StringWriter();
+        StringWriter swriter = new StringWriter();
 
         try {
             ObjectFactory cfdiFactory = new ObjectFactory();
@@ -62,6 +67,16 @@ public class FacturaXml {
             cfdi.setExportacion(req.getComprobanteAttributes().getExportacion());
             cfdi.setMetodoPago(CMetodoPago.fromValue(req.getComprobanteAttributes().getMetodoPago()));
             cfdi.setLugarExpedicion(req.getComprobanteAttributes().getLugarExpedicion());
+
+            byte[] contents = new byte[1024];
+            int bytesRead;
+            StringBuilder certContents = new StringBuilder();
+
+            while ((bytesRead = certificate.read(contents)) != -1) {
+                certContents.append(new String(contents, 0, bytesRead));
+            }
+            cfdi.setCertificado(certContents.toString());
+            cfdi.setNoCertificado(certificateNo);
 
             Comprobante.Emisor emisor = cfdiFactory.createComprobanteEmisor();
             emisor.setRfc(req.getEmisorAttributes().getRfc());
@@ -176,22 +191,37 @@ public class FacturaXml {
             }
             cfdi.setImpuestos(impuestos);
 
+            // Marshalling (without issuer signature)
+            Marshaller marshaller;
             String contextPath = "mx.gob.sat.cfd._4";
             String schemaLocation = "http://www.sat.gob.mx/cfd/4 http://www.sat.gob.mx/sitio_internet/cfd/4/cfdv40.xsd";
 
-            // Hacer el marshalling del cfdi object
             JAXBContext context = JAXBContext.newInstance(contextPath);
-            Marshaller marshaller = context.createMarshaller();
+            marshaller = context.createMarshaller();
             marshaller.setProperty("jaxb.schemaLocation", schemaLocation);
             marshaller.setProperty("com.sun.xml.bind.namespacePrefixMapper", new CfdiNamespaceMapper());
             marshaller.setProperty("jaxb.formatted.output", true);
-            marshaller.marshal(cfdi, writer);
 
-        } catch (JAXBException | DatatypeConfigurationException ex) {
-            throw new EngineError("Impossible to turn the request into the xml",
-                    ex, ErrorCodes.FORMAT_BUILDER_ISSUE);
+            marshaller.marshal(cfdi, swriter);
+
+            // Marshalling (including issuer signature)
+            var pemKeyBr = new BufferedReader(new InputStreamReader(signerKey));
+            var cfdiBr = new BufferedReader(new StringReader(swriter.toString()));
+            var xsltSource = new StreamSource(_cloader.getResourceAsStream("cfdv40/cadenaoriginal_4_0.xslt"));
+            var toolbox = new CfdiToolBox();
+            String originalStr = toolbox.renderOriginal(cfdiBr, xsltSource);
+            String sello = toolbox.signOriginal(pemKeyBr, originalStr);
+            cfdi.setSello(sello);
+
+            swriter = new StringWriter();
+            marshaller.marshal(cfdi, swriter);
+
+        } catch (JAXBException | DatatypeConfigurationException | IOException ex) {
+            throw new EngineError("An error occurred when creating cfdi xml.", ex);
         }
 
-        return writer;
+        return swriter;
     }
 }
+
+class CfdiToolBox implements IToolbox {}
