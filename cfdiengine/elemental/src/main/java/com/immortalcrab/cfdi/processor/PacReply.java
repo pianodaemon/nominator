0ba8cc3415490c79f788a8ac4a4c4aa42f315425
Edwin Plauchu <pianodaemon@gmail.com>
2023-04-09 17:51:27 -0600

    Update PacReply.java

--- cfdiengine/elemental/src/main/java/com/immortalcrab/cfdi/processor/PacReply.java
+++ cfdiengine/elemental/src/main/java/com/immortalcrab/cfdi/processor/PacReply.java
@@ -5,37 +5,14 @@ import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
 
+@AllArgsConstructor
+@Getter
+@Setter
 public class PacReply {
 
-    private final int replyCode;
+    private final @NonNull
+    StringBuilder buffer;
 
-    private final Content content;
-
-    public PacReply(int replyCode, Content content) {
-        this.replyCode = replyCode;
-        this.content = content;
-    }
-
-    public int getRc() {
-        return replyCode;
-    }
-
-    public Content getContent() {
-        return content;
-    }
-
-    @AllArgsConstructor
-    @Getter
-    @Setter
-    protected static class Content {
-
-        private final @NonNull
-        StringBuilder buffer;
-
-        private final @NonNull
-        String name;
-
-        private final @NonNull
-        String id;
-    }
+    private final @NonNull
+    String name;
 }
