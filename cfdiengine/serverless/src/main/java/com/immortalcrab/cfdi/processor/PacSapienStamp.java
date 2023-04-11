d11a0019b1fbab356bf0dc2b23114453b582b287
Edwin Plauchu <pianodaemon@gmail.com>
2023-04-11 11:20:16 -0600

    Update PacSapienStamp.java

--- cfdiengine/serverless/src/main/java/com/immortalcrab/cfdi/processor/PacSapienStamp.java
+++ cfdiengine/serverless/src/main/java/com/immortalcrab/cfdi/processor/PacSapienStamp.java
@@ -29,7 +29,7 @@ public class PacSapienStamp implements IStamp<PacReply> {
 
         CFDI client = new CFDI();
         TimbradoCFDIRequest treq = new TimbradoCFDIRequest();
-        treq.setUser(Integer.valueOf(login));
+        treq.setUser(Integer.parseInt(login));
         treq.setPass(passwd);
         // SIGNED BY CUSTOMER
         treq.setTipoPeticion(1);
