122bec83123fd8a19283b9750005c33b9cad6882
Edwin Pĺauchu <pianodaemon@gmail.com>
2023-04-12 10:22:54 -0500

    Revert "Update IssueHandler.java"
    This reverts commit bbe5dfd0421af1290206e9f0d90c321fd4f18f75.

--- cfdiengine/serverless/src/main/java/com/immortalcrab/cfdi/processor/IssueHandler.java
+++ cfdiengine/serverless/src/main/java/com/immortalcrab/cfdi/processor/IssueHandler.java
@@ -16,15 +16,14 @@ import com.amazonaws.arn.Arn;
 import com.immortalcrab.cfdi.errors.ErrorCodes;
 
 import lombok.extern.log4j.Log4j2;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.services.sqs.model.QueueDoesNotExistException;
 
 @Log4j2
 public class IssueHandler implements RequestHandler<SQSEvent, Void> {
-    
+
     @Override
     public Void handleRequest(SQSEvent event, Context context) {
-        
+
         for (SQSEvent.SQSMessage msg : event.getRecords()) {
             try {
                 handleMessage(Producer.obtainSteadyPipeline(), msg);
@@ -32,10 +31,10 @@ public class IssueHandler implements RequestHandler<SQSEvent, Void> {
                 log.error("Exception handling batch seed request.", ex);
             }
         }
-        
+
         return null;
     }
-    
+
     private void handleMessage(Producer producer, SQSEvent.SQSMessage msg) throws EngineError {
         Arn eventSourceArn = Arn.fromString(msg.getEventSourceArn());
         log.info(String.format("We've got a message to process from queue %s", eventSourceArn.getResourceAsString()));
@@ -44,11 +43,11 @@ public class IssueHandler implements RequestHandler<SQSEvent, Void> {
                 details.getName(), details.getBuffer().toString()));
         erradicateMessageFromQueue(eventSourceArn, msg.getReceiptHandle());
     }
-    
+
     private Payload percolatePayload(SQSEvent.SQSMessage msg) throws EngineError {
-        
+
         log.debug(msg.getBody());
-        
+
         AWSEvent<Payload> body;
         try {
             body = AWSEvent.unmarshallEvent(msg.getBody(), Payload.class);
@@ -58,16 +57,16 @@ public class IssueHandler implements RequestHandler<SQSEvent, Void> {
             throw new EngineError("Event could not be unmarshall", ex, ErrorCodes.REQUEST_INVALID);
         }
     }
-    
+
     private static void erradicateMessageFromQueue(Arn eventSourceArn, final String receiptHandle) throws QueueDoesNotExistException {
-        
+
         var queueName = eventSourceArn.getResourceAsString();
         Region queueRegion = Region.of(eventSourceArn.getRegion());
-        SqsClient sqsClient = SqsClient.builder().region(queueRegion).credentialsProvider(ProfileCredentialsProvider.create()).build();
-        
+        SqsClient sqsClient = SqsClient.builder().region(queueRegion).build();
+
         log.debug(String.format("Attemping to delete message from queue %s in region %s", queueName, queueRegion.toString()));
         GetQueueUrlResponse getQueueUrlResponse = sqsClient.getQueueUrl(GetQueueUrlRequest.builder().queueName(queueName).build());
-        
+
         DeleteMessageRequest deleteMessageRequest = DeleteMessageRequest.builder()
                 .queueUrl(getQueueUrlResponse.queueUrl())
                 .receiptHandle(receiptHandle)
