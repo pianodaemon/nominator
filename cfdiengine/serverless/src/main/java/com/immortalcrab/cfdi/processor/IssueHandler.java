9e4a23276418c3e71311ce2a75709bed44f7d118
Edwin Pĺauchu <pianodaemon@gmail.com>
2023-04-12 11:33:43 -0500

    Over Lambda integration with SQS
    Lambda automatically deletes the message from queue for you
    if the lambda functions returns success status.
    
    https://docs.aws.amazon.com/lambda/latest/dg/with-sqs.html
    
    Signed-off-by: Edwin Pĺauchu <pianodaemon@gmail.com>

--- cfdiengine/serverless/src/main/java/com/immortalcrab/cfdi/processor/IssueHandler.java
+++ cfdiengine/serverless/src/main/java/com/immortalcrab/cfdi/processor/IssueHandler.java
@@ -7,16 +7,10 @@ import com.immortalcrab.cfdi.helpers.Payload;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
-import software.amazon.awssdk.services.sqs.SqsClient;
-import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
-import software.amazon.awssdk.services.sqs.model.GetQueueUrlRequest;
-import software.amazon.awssdk.services.sqs.model.GetQueueUrlResponse;
-import software.amazon.awssdk.regions.Region;
 import com.amazonaws.arn.Arn;
 import com.immortalcrab.cfdi.errors.ErrorCodes;
 
 import lombok.extern.log4j.Log4j2;
-import software.amazon.awssdk.services.sqs.model.QueueDoesNotExistException;
 
 @Log4j2
 public class IssueHandler implements RequestHandler<SQSEvent, Void> {
@@ -41,7 +35,6 @@ public class IssueHandler implements RequestHandler<SQSEvent, Void> {
         var details = producer.doIssue(percolatePayload(msg));
         log.debug(String.format("Issue for %s is attained {%s}",
                 details.getName(), details.getBuffer().toString()));
-        erradicateMessageFromQueue(eventSourceArn, msg.getReceiptHandle());
     }
 
     private Payload percolatePayload(SQSEvent.SQSMessage msg) throws EngineError {
@@ -56,21 +49,5 @@ public class IssueHandler implements RequestHandler<SQSEvent, Void> {
         } catch (IOException ex) {
             throw new EngineError("Event could not be unmarshall", ex, ErrorCodes.REQUEST_INVALID);
         }
-    }
-
-    private static void erradicateMessageFromQueue(Arn eventSourceArn, final String receiptHandle) throws QueueDoesNotExistException {
-
-        var queueName = eventSourceArn.getResourceAsString();
-        Region queueRegion = Region.of(eventSourceArn.getRegion());
-        SqsClient sqsClient = SqsClient.builder().region(queueRegion).build();
-
-        log.debug(String.format("Attemping to delete message from queue %s in region %s", queueName, queueRegion.toString()));
-        GetQueueUrlResponse getQueueUrlResponse = sqsClient.getQueueUrl(GetQueueUrlRequest.builder().queueName(queueName).build());
-
-        DeleteMessageRequest deleteMessageRequest = DeleteMessageRequest.builder()
-                .queueUrl(getQueueUrlResponse.queueUrl())
-                .receiptHandle(receiptHandle)
-                .build();
-        sqsClient.deleteMessage(deleteMessageRequest);
     }
 }
